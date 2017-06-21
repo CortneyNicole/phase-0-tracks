0c418975ff5f01c1afa7e208a581f7a1164aec25
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-21 15:45:09 -0700

    pair 4.3 release 1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+
+
+  puts "what's your name?"
+  name = gets.chomp
+
+  puts "How loud are you from 1-10?"
+  volume_level = gets.chomp
+
+  puts "What's your fur color?"
+  fur_color = gets.chomp
+
+  puts "Are you a good canidate for adoption?(yes/no)"
+  adoptable = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp
+
+if age == ""
+   age = nil
+end
+
