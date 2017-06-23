b83009cc02f1f8818eee0888ae7f35536ac7c12a
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-22 23:26:02 -0700

    got rid of duplicated code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,5 @@
 puts "What's your name?"
 name = gets.chomp
-puts "How old are you and in what year were you born?"
-age = gets.chomp
-puts "Our cafeteria serves garlic bread. Do you want us to set an order for you?"
-garlic_bread = gets.chomp
-puts "would you like to enroll in our company's health insurence?"
-insurence = gets.chomp
-
-
-puts "What's your name?"
-name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
 puts "in what year were you born?(****)"
