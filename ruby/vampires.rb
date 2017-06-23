08bedbd6b3ac0320d86ae0f7c6b0428d92188a4c
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-22 21:30:08 -0700

    added employment quiz. finished release1

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+name = gets.chomp
+puts "How old are you and in what year were you born?"
+age = gets.chomp
+birth_year = gets.chomp
+puts "Our cafeteria serves garlic bread. Do you want us to set an order for you?"
+garlic_bread = gets.chomp
+puts "would you like to enroll in our company's health insurence?"
+insurence = gets.chomp
+
+
