faa547a881e5ffdbfcef199b3b53aab575834208
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-23 12:37:01 -0700

    fixed logic and loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,44 +1,51 @@
-puts "What's your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "in what year were you born?(****)"
-year_born = gets.chomp.to_i
-puts "Our cafeteria serves garlic bread. Do you want us to set an order for you?(yes/no)"
-garlic_bread = gets.chomp
-puts "would you like to enroll in our company's health insurence?(yes/no)"
-insurence = gets.chomp
+puts "How many employees will be processed?"
+employee_number = gets.chomp.to_i
 
-employee_age = 2017 - year_born
-if age == employee_age
+until employee_number == 0
+  puts "What's your name?"
+  name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  puts "in what year were you born?(****)"
+  year_born = gets.chomp.to_i
+  puts "Our cafeteria serves garlic bread. Do you want us to set an order for you?(yes/no)"
+  garlic_bread = gets.chomp
+  puts "would you like to enroll in our company's health insurence?(yes/no)"
+  insurence = gets.chomp
+
+  employee_age = 2017 - year_born
+  if age == employee_age
      age = true
-else 
-    age = false 
-end
-
-if garlic_bread == "yes"
-     garlic_bread = true
-end
-
-if insurence == "yes"
-     insurence = true
-end 
-
-  if age && (garlic_bread || insurence) == true
-        puts "Probably Not a Vampire"
-          elsif age && (garlic_bread || insurence) == false
-                puts "Probably a Vampire"
-                  elsif age && (garlic_bread && insurence) == false
-                        puts "Almost Certainly a Vampire"
-                          elsif name == "Drake Cula" || "Tu Fang"
-                                puts "VAMPIRE!!!"
-                                  else 
-                                        puts "Results Inconclusive"
-                                          end
-
-
-
-
-
+  else 
+     age = false   
   end
+
+  if garlic_bread == "yes"
+     garlic_bread = true
+  else 
+    garlic_bread = false
+  end
+
+  if insurence == "yes"
+     insurence = true
+  else 
+    insurence =false
+  end 
+
+  if name == "Drake Cula" || name == "Tu Fang"
+     name = true
+  else 
+     name = false
+  end
+
+  if name 
+    puts "VAMPIRE!!!!"
+  elsif !age && (!garlic_bread && !insurence)
+    puts "Almost Certainly a Vampire!"
+  elsif !age && (!garlic_bread || !insurence)
+    puts "Probably a Vampire"
+  elsif age && (garlic_bread || insurence)
+    puts "Probably Not a Vampire!"
+  end
+  employee_number -= 1
 end
