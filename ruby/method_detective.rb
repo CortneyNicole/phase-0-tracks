2b5a3b9285d599cf195a2e79be2c3030ad2b221e
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-27 10:56:11 -0700

    finished 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -16,24 +16,26 @@
 # "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".insert(9, " suspect")
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
 # "The case of the disappearing last letter".slice!(0..38)
 # => "The case of the disappearing last lette"
 
+"The case of the disappearing last letter".chop
+
 # "The mystery of the missing first letter".slice!(1..39)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
