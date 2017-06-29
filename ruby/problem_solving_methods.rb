d3dde5984d011d1c68cbc63a8ed9a78f84dec6b2
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-29 16:46:51 -0700

    finish release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,11 +1,29 @@
+=begin
+* create array
+*write method that goes through the array and find the called number
+*return the index
+
+=end
+
+
 array = [14, 24, 34, 44, 54]
 
 def search(array, n)
+integer = nil
     array.length.times do |i|
       if array[i] == n
+        integer = i
       end
     end
-    p integer
+  p integer
 end
 
- search(array, 33)
+search(array, 44)
+
+
+
+
+
+
+
+
