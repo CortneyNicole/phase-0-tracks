ee3e1da83041805edbcd832215e3ba4707c72d00
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-30 13:02:29 -0700

    finish release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,7 +5,7 @@
 
 =end
 
-*release 1
+*release 0
 array = [14, 24, 34, 44, 54]
 
 def search(array, n)
@@ -20,7 +20,7 @@ end
 
 search(array, 44)
 
-#release 2
+#release 1
 =begin
 *initiate array that is empty but intialize with 100
 *creater method that starts w 0 and 1 set to variables and add
@@ -49,8 +49,47 @@ end
 
 p fib_array
 
+#release 2
 
+s# bubble sort
 
+# for each index in the array, the item will compare to see
+# if it is less or greater than the value and continue
+# on until the elements in the array have been sorted
+
+array = [1,4,7,9,4,3,5,7]
+
+def bubble_sort(array)
+  number = array.length
+
+  loop do
+
+    sorted = false
+    # while this array is NOT sorted
+
+    (number-1).times do |i|
+        # starting from the 2nd index, it will compare to the first index
+        # if the element is greater than the next element
+        if array[i] > array[i+1]
+
+          # then we want to switch the two elements in the array index
+          array[i], array[i+1] = array[i+1], array[i]
+
+          # it will continue to swap the element until reaches the
+          # end of the array or its proper place
+          sorted = true
+        end
+    end
+
+    break if not sorted
+      # break after the array has been sorted
+  end
+
+  # output the sorted array
+  array
+end
+
+bubble_sort(array)
 
 
 
