33b2bb98eeab0d5ef18426e95f98e16983d7b263
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-30 12:01:52 -0700

    finish release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,7 +5,7 @@
 
 =end
 
-
+*release 1
 array = [14, 24, 34, 44, 54]
 
 def search(array, n)
@@ -19,6 +19,27 @@ integer = nil
 end
 
 search(array, 44)
+
+#release 2
+fib_array = Array.new
+
+def fibonacci(n)
+  a = 0
+  b = 1
+  n.times do
+    temp_i = a
+    a = b
+    b = temp_i + b
+  end
+  return a
+
+end
+
+50.times do |n|
+  fib_array.push(fibonacci(n))
+end
+
+p fib_array
 
 
 
