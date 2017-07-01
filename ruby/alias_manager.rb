97375c9fae04af73adbf65df4e0b56812e532521
Cortney Scott <cortneynicolescott@gmail.com>
2017-06-30 17:38:04 -0700

    create method to for release 0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,10 @@
-=begin
-*declare method that takes parameters of first and last name
-    def alias_manager(name)
-*
-=end
-
 def alias_manager(first_name, last_name)
 
   name = last_name + " " + first_name
 
-  vowels_hash = {
+#bcdfghjklmnpqrstvwxyz
+#aeiou
+  alphabet_hash = {
   'a' => 'e',
   'A' => 'E',
   'e' => 'i',
@@ -18,11 +14,51 @@ def alias_manager(first_name, last_name)
   'o' => 'u',
   'O' => 'U',
   'u' => 'a',
-  'U' => 'A'
+  'U' => 'A',
+  'b' => 'c',
+  'B' => 'C',
+  'c' => 'd',
+  'C' => 'D',
+  'd' => 'f',
+  'D' => 'F',
+  'f' => 'g',
+  'F' => 'G',
+  'g' => 'h',
+  'G' => 'H',
+  'h' => 'j',
+  'H' => 'J',
+  'j' => 'k',
+  'J' => 'K',
+  'k' => 'l',
+  'K' => 'L',
+  'l' => 'm',
+  'L' => 'M',
+  'm' => 'n',
+  'M' => 'N',
+  'n' => 'p',
+  'N' => 'P',
+  'p' => 'q',
+  'P' => 'Q',
+  'q' => 'r',
+  'Q' => 'R',
+  'r' => 's',
+  'R' => 'S',
+  's' => 't',
+  'S' => 'T',
+  't' => 'v',
+  'T' => 'V',
+  'v' => 'w',
+  'V' => 'W',
+  'w' => 'x',
+  'W' => 'X',
+  'x' => 'y',
+  'X' => 'Y',
+  'y' => 'z',
+  'Y' => 'Z'
 }
 
   name_array = name.split('').map do |character|
-    vowels_hash[character] || character
+    alphabet_hash[character] || character
   end
   name_array.join
 end
