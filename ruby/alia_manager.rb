d6071e37057399b587e58feaba5a8249639256bd
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-02 17:48:24 -0700

    added pseudocode

--- ruby/alia_manager.rb
+++ ruby/alia_manager.rb
@@ -1,11 +1,28 @@
+=begin
+* create method that takes first and last name arguement
+* creat step in method that combines first and last and swaps it
+* add hash that rotates vowels to next vowels and consonants to next consonants
+* make sure hash has capitols
+* create array that will store letters of name
+* create method that seperates letter
+* write loop that iterate through array and changes letters
+* combine array to make code name
+* create new hash to store namew
+* create user interface
+* loop through interface
+=end
+
 
 #creates function that takes first and last name as argument and swaps them.
+
 def make_alias(first_name, last_name)
 
   name = last_name + " " + first_name
-#hash for alphabet
-#bcdfghjklmnpqrstvwxyz
-#aeiou
+
+# hash for alphabet
+# bcdfghjklmnpqrstvwxyz
+# aeiou
+
   alphabet_hash = {
   'a' => 'e',
   'A' => 'E',
@@ -59,17 +76,22 @@ def make_alias(first_name, last_name)
   'Y' => 'Z'
 }
 #split name into array and iterates through array and alphabet hash changing vowels to next vowels and  consonant
-  name_array = name.split('').map do |character|
+
+name_array = name.split('').map do |character|
     alphabet_hash[character] || character
   end
+
   # joins name array into string
+
   return name_array.join
 end
 
 #creates a hash to store names and alias
+
 code_names = Hash.new
 
 #user interface loop. loops through input names until quit is entered and exits loop
+
 while true
   puts "Please enter a first name. "
   first_name =gets.chomp
