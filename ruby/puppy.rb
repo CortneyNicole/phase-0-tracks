5031bd25eb71a230108858848e46f251950e91fe
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-11 15:15:15 -0700

    completed release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+SPECIES ------------------------
+puppies
+
+CHARACTERISTICS ----------------
+fluffiness: varies
+Friendly: yes
+Number of legs: 4
+Name: varies
+Color: varies
+Cute: yes
+Allergins: varies
+
+BEHAVIOR -----------------------
+Bark
+Lick
+Roll
+Shake
+=end
