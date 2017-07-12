c854970b8247ca3955a6015588aad2773f4747ba
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-12 11:42:11 -0700

    finish release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    puts "Woof!" * number
+  end
+
+  def roll_over
+    puts "*Roll Over*"
+  end
+
+  def dog_years(human_years)
+    dog = human_years * 7
+    p "your dog age is #{dog}!"
+  end
+
+  def sit
+    puts "*sit*"
+  end
+
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+
+fido.speak(5)
+
+fido.roll_over
+
+fido.dog_years(2)
+
+fido.sit
