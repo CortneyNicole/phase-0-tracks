ed821049cfe4354acf5cfa3e3aa00e28cbc93a0e
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-15 09:19:09 -0700

    finish release 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -16,6 +16,28 @@
 
 module Shout
 
-  def
+  def frustrated(str)
+    puts str.upcase + "?!?!?!" + "**pulls hair out**"
+  end
+
+  def celebrating(str)
+    puts str.upcase + "!!!!!" + "**happy dance**"
+  end
 
 end
+
+class Sports_fan
+  include Shout
+end
+
+class Programming_student
+  include Shout
+end
+
+bill = Sports_fan.new
+bill.frustrated("What are you doing")
+bill.celebrating("score")
+
+cortney = Programming_student.new
+cortney.frustrated("Why isn't this working")
+cortney.celebrating("I did it")
