7072b2fe9c5324c5d4ea60b4439528f7631beddd
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-16 21:35:45 -0700

    finish game and and test. need to add driver code

--- GuessingGame/game.rb
+++ GuessingGame/game.rb
@@ -2,12 +2,33 @@ class GuessingGame
 attr_reader :word, :guessed_letters, :guess_count, :guest_count, :guess_limit, :guess_state, :won, :over
 
   def initialize
-    @word = ""
+    @word = []
     @guessed_letters = []
     @guess_count = 0
     @guess_limit = 0
-    @guess_state = "_ "
+    @guess_state = "_"
     @won         = false
     @over        = false
+  end
+
+  def get_word(word)
+    @word = word.split("")
+    @guess_limit = @word.length - 1
+    @guess_state = "_" * word.length
+  end
+
+  def check_letter(letter)
+    if !@guessed_letters.include?(letter)
+      @guessed_letters.push(letter)
+      @guess_count += 1
+    end
+
+    if @guess_limit == @guess_count
+      @over = true
+    end
+
+    if (@guessed_letters & @word) == @word
+      @won = true
+    end
   end
 end
--- GuessingGame/game_spec.rb
+++ GuessingGame/game_spec.rb
@@ -1,22 +1,64 @@
 require_relative 'game'
 
 describe GuessingGame do
+  let(:game) {GuessingGame.new}
+
   it 'initialize with word, guessed_letters, guess_count, guess_limit, guess_state, won, over' do
-    g = GuessingGame.new
-    expect(g.word).to eq ""
-    expect(g.guessed_letters).to eq []
-    expect(g.guess_count).to eq 0
-    expect(g.guess_limit).to eq 0
-    expect(g.guess_state).to eq "_ "
-    expect(g.won).to eq false
-    expect(g.over).to eq false
+    expect(game.word).to eq []
+    expect(game.guessed_letters).to eq []
+    expect(game.guess_count).to eq 0
+    expect(game.guess_limit).to eq 0
+    expect(game.guess_state).to eq "_"
+    expect(game.won).to eq false
+    expect(game.over).to eq false
   end
+
   it 'accept a word' do
+    game.get_word("word")
+    expect(game.word).to eq ["w", "o", "r", "d"]
+  end
 
+  it 'checks letter' do
+    game.get_word("word")
+    game.check_letter("w")
+    expect(game.guessed_letters).to eq ["w"]
+  end
 
-  it 'keeps track of progress'
-  it 'keep count of guesses'
-  it 'has guess limit related to length of word'
-  it 'wins if user guesses the correct word'
-  it 'is over if user runs out of guesses'
+  it 'keeps track of progress' do
+      game.get_word("word")
+      expect(game.guess_state).to eq "____"
+  end
+
+  it 'keep count of guesses' do
+     game.get_word("word")
+    game.check_letter("w")
+    game.check_letter("x")
+    expect(game.guess_count).to eq 2
+  end
+
+  it 'has guess limit related to length of word' do
+    game.get_word("word")
+    expect(game.guess_limit).to eq 3
+  end
+
+  it 'wins if user guesses the correct word' do
+    game.get_word("word")
+    game.check_letter("w")
+    game.check_letter("o")
+    game.check_letter("r")
+    game.check_letter("d")
+    expect(game.won).to eq true
+  end
+
+  it 'is over if user runs out of guesses' do
+    game.get_word("word")
+    game.check_letter("w")
+    expect(game.guessed_letters).to eq ["w"]
+    game.check_letter("w")
+    expect(game.guessed_letters).to eq ["w"]
+    game.check_letter("q")
+    game.check_letter("x")
+    expect(game.over).to eq true
+  end
+
 end
