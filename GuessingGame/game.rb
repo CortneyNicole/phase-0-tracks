6e9e96dbd734f2ea20e91645707d4ea4fd904dea
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-16 20:31:27 -0700

    initialize and tested code

--- GuessingGame/game.rb
+++ GuessingGame/game.rb
@@ -1,1 +1,13 @@
 class GuessingGame
+attr_reader :word, :guessed_letters, :guess_count, :guest_count, :guess_limit, :guess_state, :won, :over
+
+  def initialize
+    @word = ""
+    @guessed_letters = []
+    @guess_count = 0
+    @guess_limit = 0
+    @guess_state = "_ "
+    @won         = false
+    @over        = false
+  end
+end
--- GuessingGame/game_spec.rb
+++ GuessingGame/game_spec.rb
@@ -1,1 +1,22 @@
-require_reletive 'game'
+require_relative 'game'
+
+describe GuessingGame do
+  it 'initialize with word, guessed_letters, guess_count, guess_limit, guess_state, won, over' do
+    g = GuessingGame.new
+    expect(g.word).to eq ""
+    expect(g.guessed_letters).to eq []
+    expect(g.guess_count).to eq 0
+    expect(g.guess_limit).to eq 0
+    expect(g.guess_state).to eq "_ "
+    expect(g.won).to eq false
+    expect(g.over).to eq false
+  end
+  it 'accept a word' do
+
+
+  it 'keeps track of progress'
+  it 'keep count of guesses'
+  it 'has guess limit related to length of word'
+  it 'wins if user guesses the correct word'
+  it 'is over if user runs out of guesses'
+end
