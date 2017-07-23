9dca3633a1c367856097f2181fdd10caffbe0cf2
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-23 14:41:42 -0700

    changing gears for a bit

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,7 @@
 
-
-
-var wordArray = ["crazy", "cat", "lady", "love", "coffee"];
+//Release 0
+//var wordArray = ["crazy", "cat", "lady", "love", "coffee"];
+var wordArray = ["stress", "bootcamp", "confusion", "excitement"];
 
 var longest = "";
 
@@ -10,5 +10,28 @@ for(i = 0; i < wordArray.length; i++) {
     longest =wordArray[i];
   }
 }
+
+//Release 2
+var gemmaCat = {name: "Maddi", adoptionAge: "10 months", age: 4};
+var maddiCat = {name: "Gemma", adoptionAge: "10 months", age: 2}
+
+function matchObjects(maddiCat, gemmaCat)
+  //loop through each object
+  //find if any key-value pairs match
+  //if any match return true
+  //else return false
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log(longest);
