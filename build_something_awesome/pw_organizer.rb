5c6b705fcb520130b22097b073c9db1adc31bdbb
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-30 20:37:00 -0700

    started assignment

---
+++ build_something_awesome/pw_organizer.rb
@@ -0,0 +1,18 @@
+require 'sqlite3'
+require 'faker'
+
+#create database
+db = SQLite3::Database.new("passwords-organizer.db")
+db.results_as_hash = true
+
+#create a create command in SQL
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS pw-organizer(
+  id INTEGER PRIMARY KEY,
+  website VARCHAR(255),
+  password VARCHAR(255)
+  )
+SQL
+
+#create the phonebook table
+db.execute(create_table_cmd)
