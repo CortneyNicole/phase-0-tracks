5fdf5c7187746532614d95aeb0cb50ca4e64228a
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-31 07:56:41 -0700

    created table

--- build_something_awesome/pw_organizer.rb
+++ build_something_awesome/pw_organizer.rb
@@ -4,11 +4,10 @@ require 'faker'
 
 #create database
 db = SQLite3::Database.new("passwords-organizer.db")
-db.results_as_hash = true
 
 #create a create command in SQL
 create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS pw-organizer(
+  CREATE TABLE IF NOT EXISTS pw_organizer(
   id INTEGER PRIMARY KEY,
   website VARCHAR(255),
   password VARCHAR(255)
@@ -18,5 +17,6 @@ SQL
 #create the phonebook table
 db.execute(create_table_cmd)
 
-# def
-# end
+def add_pw_site(website, password)
+  db.execute("INSERT INTO pw_organizer(website, password) VALUES (?, ?)", [website, password])
+end
