3f39e83d551128d8fe6fb647caacac7cc383b826
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-31 12:04:58 -0700

    created table

--- build_something_awesome/pw_organizer.rb
+++ build_something_awesome/pw_organizer.rb
@@ -1,7 +1,15 @@
 require 'sqlite3'
 require 'faker'
 
-SQLite3::Database.new( "pw_organizer.db" )
+db = SQLite3::Database.new( "pw_organizer.db" )
 
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS organizer(
+    id INTEGER PRIMARY KEY,
+    domain_name VARCHAR(255),
+    password VARCHAR(255)
+  )
+SQL
 
+db.execute(create_table_cmd)
 
