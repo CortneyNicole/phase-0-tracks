d8a57d947900504432668e4a3f1c1a69aed2d7a3
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-31 08:58:23 -0700

    create data base, included update delete and add

--- build_something_awesome/pw_organizer.rb
+++ build_something_awesome/pw_organizer.rb
@@ -3,7 +3,7 @@ require 'faker'
 
 
 #create database
-db = SQLite3::Database.new("passwords-organizer.db")
+pw_db = SQLite3::Database.new("passwords-organizer.db")
 
 #create a create command in SQL
 create_table_cmd = <<-SQL
@@ -14,9 +14,34 @@ create_table_cmd = <<-SQL
   )
 SQL
 
-#create the phonebook table
-db.execute(create_table_cmd)
+#create the password table
+pw_db.execute(create_table_cmd)
 
+#add new password and website pair to data base
 def add_pw_site(website, password)
-  db.execute("INSERT INTO pw_organizer(website, password) VALUES (?, ?)", [website, password])
+  pw_db.execute("INSERT INTO pw_organizer(website, password) VALUES (?, ?)", [website, password])
+end
+
+#update password
+def update_pw(db, website, password)
+  db.execute("UPDATE pw_organizer SET password=#{password} WHERE website='#{website}'")
+end
+
+#delete password
+def delete_pw_site(pw_db)
+  puts "which website abd password would you like to delete"
+  name = gets.chomp
+  if current(pw_db, website)
+    db.execute("DELETE FROM pw_organizer WHERE website='#{website}'")
+    return_all(pw_db)
+  else
+    puts "#{website} is not in your phonebook"
+  end
+end
+
+#add website pw pair
+def add_pw_site(db, website, password)
+  db.execute("INSERT INTO pw_organizer (password, website) VALUES (?, ?)", [password, website])
+  puts return_website(pw_db, website)
+  run_pw_organizer(pw_db)
 end
