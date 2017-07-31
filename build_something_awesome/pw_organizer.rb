2efb6d5a21d258918e7c7e13b4a381f5b4de821f
Cortney Scott <cortneynicolescott@gmail.com>
2017-07-31 13:21:37 -0700

    made 10 rancdom

--- build_something_awesome/pw_organizer.rb
+++ build_something_awesome/pw_organizer.rb
@@ -13,3 +13,20 @@ SQL
 
 db.execute(create_table_cmd)
 
+#db.execute("INSERT INTO organizer (domain_name, password) VALUES ('facebook.com', 'rainbow')")
+
+def create_password(db, domain_name, password)
+  db.execute("INSERT INTO organizer (domain_name, password) VALUES (?, ?)", [domain_name, password])
+end
+
+# 10.times do
+#   create_password(db, Faker::Internet.domain_name, Faker::Internet.password)
+# end
+
+
+
+
+# passwords = db.execute("SELECT * FROM organizer")
+# passwords.each do |pw|
+#   puts "domain name:#{pw[1]} password:#{pw[2]}"
+# end
